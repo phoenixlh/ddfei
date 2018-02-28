c0b4f63e92a01df61e266cee050e610875ccf4cc
lihong <1224954159@qq.com>
2018-02-28 22:57:43 +0800

    ye

--- jy/sta.js
+++ jy/sta.js
@@ -1,12 +1,29 @@
 window.onload = function(){
+//	列表隐藏于显示
 	var open = true;
 	$(".ico1").click(function(){
 		if(open){
 			$(".di").css("display","block");
 			open = false;
+			
+			$(".menu-item").click(function(evt){
+				var e = evt || window.event;
+				e.stopPropagation();
+				$(".di").css("display","block");
+			})
 		}else{	
 			$(".di").css("display","none");
 			open = true;
 		}
-	})
+	});
+	
+//	tab切换
+$(".dd li").click(function(){
+	$(this).css("background","white")
+		   .siblings()
+		   .css("background","#ccc");
+	$(".xx1").eq( $(this).index() ).css("display","block")
+	         .siblings()
+	         .css("display","none")
+})
 }
