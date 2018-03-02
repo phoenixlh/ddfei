6e4761b8bb636058959ad9f1d561542d4a406781
lihong <1224954159@qq.com>
2018-03-02 22:43:58 +0800

    ai

--- jy/routes/index.js
+++ jy/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var multiparty = require('multiparty');
 
 var UserModel = require("../model/UserModel");
+var GoodsModel = require("../model/GoodsModel");
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -17,6 +18,39 @@ router.get('/right', function(req, res, next) {
   res.render('right');
 });
 
+router.get('/list', function(req, res) {
+//		GoodsModel.find(function(err,docs){
+			var pageNo = parseInt(req.query.pageNo) || 1;
+	 	var count = parseInt(req.query.count) || 3;
+	 	
+	 	var query = GoodsModel.find({}).skip( (pageNo-1)*count ).limit(count).sort({date:-1});
+	 	query.exec(function(err,results){
+	 		res.render('list',{list:results,pageNo:pageNo,count:count});
+	 	})
+//			res.render('list',{list:docs});			
+//		})
+});
+
+router.get('/api/goodslist',function(req,res){
+	var goodsname = req.query.goodsname;
+	GoodsModel.find({goodsname:goodsname},function(err,docs){
+	  
+//		res.render('search',{list:docs});
+		var result = {
+			status:1,
+			message:"查询成功"
+		}
+		if(!err && docs.length > 0){
+			console.log("查询成功");
+			res.send(result);
+		}else{
+			console.log("查询失败");
+			result.status = -118;
+			result.message = "查询失败";
+			res.send(result);
+		}
+	})
+})
 
 router.get('/goods_add', function(req, res, next) {
   res.render('goods_add');
@@ -31,7 +65,7 @@ router.post('/api/login',function(req,res){
 		message:"登录成功"
 	}
 	UserModel.find({username:username,pwd:pwd},function(err,docs){
-		console.log(docs.length);
+//		console.log(docs.length);
 		if(!err && docs.length > 0 ){
 			console.log("登录成功");
 			res.send(result);
@@ -44,10 +78,6 @@ router.post('/api/login',function(req,res){
 	})
 });
 
-router.get('/goods_add', function(req, res, next) {
-  res.render('/goods_add', { title: '登录页面' });
-});
-
 router.post('/api/add_goods',function(req,res){
 	 var form = new multiparty.Form({
 	 	uploadDir:"./public/js"
@@ -57,11 +87,29 @@ router.post('/api/add_goods',function(req,res){
 	 	var goodsname = body.goodsname[0];
 	 	var goodsnumber = body.goodsnumber[0];
 	 	var goodsprice = body.goodsprice[0];
-	 	console.log(files);
-	 	res.send("文件上传成功");
-
+	 	
+	 	var gm = new GoodsModel();
+	 	gm.goodsname = goodsname;
+	 	gm.goodsnumber = goodsnumber;
+	 	gm.goodsprice = goodsprice;
+	 	console.log(goodsname,goodsnumber,goodsprice);
+//		console.log(files);
+	 	gm.save(function(err){
+	 		var result = {
+	 			status : 66,
+	 			message : "ok"
+	 		}
+	 		if(!err){
+	 			console.log("商品保存成功");
+	 			res.send(result);
+	 		}else{
+	 			console.log("文件上传失败");
+	 			result.status = -333;
+	 			result.message = "商品保存失败";
+	 			res.send(result);
+	 		}
+	 	})
 });
 })
-
 
 module.exports = router;
